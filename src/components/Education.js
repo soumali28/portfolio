d626b223374eb417d082ccbfffc50b48d63ec165
soumali28 <gorai.soumali@gmail.com>
2023-08-27 20:50:38 +0530

    education update

--- src/components/Education.js
+++ src/components/Education.js
@@ -5,18 +5,21 @@ import { ThemeProvider } from "styled-components";
 import LogoComponent from "../subComponents/LogoComponent";
 import PowerButton from "../subComponents/PowerButton";
 import Socials from "../subComponents/Socials";
+import { FaGraduationCap, FaSchool } from "react-icons/fa";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
 import { EducationData } from "../data/Educationdata";
 import ParticleComponent from "../subComponents/ParticleComponent";
 
+import "react-vertical-timeline-component/style.min.css";
 import Sou from "../assets/Images/about.jpg";
-import EducationCompo from "../subComponents/EducationCompo";
 
 // style
 const MainContainer = styled.div`
   background-color: ${(props) => props.theme.body};
-  width: 100%;
-  padding: 200px 0 100%;
-  position: relative;
+  padding: 200px 0 700px;
   display: flex;
   justify-content: center;
   align-items: center;
@@ -25,7 +28,7 @@ const MainContainer = styled.div`
     position: absolute;
     left: 0;
     width: 50%;
-    height: 3px;
+    height: 4px;
     background-color: #ebd8b7;
     z-index: 1;
 
@@ -39,29 +42,39 @@ const MainContainer = styled.div`
       font-family: "Dancing Script", cursive;
     }
   }
-  .vertical {
-    position: absolute;
-    transform: rotate(90deg);
-    top: 50%;
-    width: 100%;
-    height: 3px;
-    background-color: #ebd8b7;
-    z-index: 1;
-  }
   .containerScl {
-    width: 88%;
     position: absolute;
-    top: calc(25% + 1.2rem);
-    left: 0;
-    right: 0;
-    //right: calc(5% + 0.8rem);
+    width: 100%;
+    top: calc(50% + 9.2rem);
     z-index: 3;
+  }
+  .line {
+    color: #ebd8b7;
+  }
+  .board {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-top: 10px;
+
+    & > :first-child {
+      font-size: 22px;
+    }
+  }
+  @media screen and (max-width: 528px) {
+    padding: 10px 0 70rem;
+    .horizontal {
+      display: none;
+    }
+    .containerScl {
+      top: calc(30% + 1.2rem);
+    }
   }
 `;
 
 const Image = styled.div`
-  width: 12rem;
-  z-index: 3;
+  width: 13rem;
+  z-index: 5;
   & > :first-child {
     width: 100%;
     height: 100%;
@@ -69,10 +82,36 @@ const Image = styled.div`
     border-radius: 30px;
     float: right;
   }
+
+  @media screen and (max-width: 528px) {
+    display: none;
+  }
 `;
 
 // code
 const Education = () => {
+  let circle = {
+    padding: "20px",
+    background: "rgba(62, 37, 25, 0.35)",
+    borderRadius: "50%",
+    boxShadow: "0 4px 30px rgba(0, 0, 0, 0.4)",
+    backdropFilter: "blur(10px)",
+    webkitBackdropFilter: "blur(10px)",
+    border: "1px solid rgba(62, 37, 25, 0.03)",
+    color: "#ebd8b7",
+  };
+  let content = {
+    padding: "20px",
+    background: "rgba(235, 216, 183, 0.18)",
+    borderRadius: "20px",
+    boxShadow: "0 4px 30px rgba(0, 0, 0, 0.1)",
+    backdropFilter: "blur(2.1px)",
+    webkitBackdropFilter: "blur(2.1px)",
+    border: "1px solid rgba(235, 216, 183, 0.3)",
+    borderBottom: "2px solid #ebd8b7",
+    color: "#ebd8b7",
+  };
+
   return (
     <ThemeProvider theme={darkTheme}>
       <MainContainer>
@@ -87,12 +126,33 @@ const Education = () => {
         <Image>
           <img src={Sou} alt="profile"></img>
         </Image>
-        <div className="vertical"></div>
         <div className="containerScl">
-          {EducationData.map((props) => {
-            return <EducationCompo key={props.id} active={props} />;
-          })}
-          {/* <EducationCompo /> */}
+          <VerticalTimeline className="line">
+            {EducationData.map((props) => {
+              return (
+                <VerticalTimelineElement
+                  className="vertical-timeline-element--work"
+                  contentStyle={content}
+                  contentArrowStyle={{
+                    borderRight: "7px solid  #ebd8b7",
+                  }}
+                  date={props.date}
+                  iconStyle={circle}
+                  icon={
+                    props.type === "school" ? <FaSchool /> : <FaGraduationCap />
+                  }
+                >
+                  <h3 className="vertical-timeline-element-title">
+                    {props.school}
+                  </h3>
+                  <div className="board">
+                    <span>{props.marks}</span>
+                    <span>{props.board}</span>
+                  </div>
+                </VerticalTimelineElement>
+              );
+            })}
+          </VerticalTimeline>
         </div>
       </MainContainer>
     </ThemeProvider>
--- src/data/Educationdata.js
+++ src/data/Educationdata.js
@@ -1,20 +1,28 @@
 export const EducationData = [
   {
     id: 1,
-    school: "Assembly Of God Church School Asansol",
-    marks: "94%",
-    board: "ICSE",
+    school: "Asansol Engineering College",
+    marks: "8.91/10",
+    board: "MAKAUT",
+    type: "university",
+    date: "2021-2025",
   },
   {
     id: 2,
     school: "St Patrick's School Asansol",
     marks: "89.9%",
     board: "ISC",
+    type: "school",
+    date: "2019-2021",
   },
   {
     id: 3,
-    school: "Asansol Engineering College",
-    marks: "8.91/10",
-    board: "MAKAUT",
+    school: "Assembly Of God Church School Asansol",
+    marks: "94%",
+    board: "ICSE",
+    type: "school",
+    date: "2006-2019",
   },
+
+  
 ];
