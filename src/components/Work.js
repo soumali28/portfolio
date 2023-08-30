eca0d695009fa41ce99f8a2263fc2b9fd2074f4f
soumali28 <gorai.soumali@gmail.com>
2023-08-30 23:24:49 +0530

    work section modification

--- src/components/Work.js
+++ src/components/Work.js
@@ -1,43 +1,35 @@
 import React from "react";
 import styled from "styled-components";
-import Girl from "../assets/Images/women.png"
 import LogoComponent from "../subComponents/LogoComponent";
 import Socials from "../subComponents/Socials";
 import PowerButton from "../subComponents/PowerButton";
 import { darkTheme, lightTheme } from "./Themes";
-import { Line } from "./Allsvg";
 
+import Work1 from "../assets/Images/work1.png";
+import Work2 from "../assets/Images/work2.png";
+import Work3 from "../assets/Images/work3.png";
+import Work4 from "../assets/Images/work4.png";
+import Work5 from "../assets/Images/work5.png";
 
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from "react-responsive-carousel";
 
 // Styling
 const MainContainer = styled.div`
   width: 100%;
-  height: 100vh;
   background-color: ${(props) =>
     props.theme === "dark" ? darkTheme.body : lightTheme.body};
   position: relative;
   display: flex;
   justify-content: center;
   flex-direction: column;
-`;
-const Ground = styled.div`
-  position: absolute;
-  bottom: 10%;
-  width: 100%;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-`;
-const User = styled.div`
-  position: absolute;
-  bottom: 10%;
-  width: 10%;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
 
-  img{
-    width: 100%;
+  .carouselContainer {
+    margin: 120px 40px 40px;
+    border-radius: 30px;
+  }
+  .carouselImg{
+    border-radius: 20px;
   }
 `;
 
@@ -48,16 +40,30 @@ function Work(props) {
       <LogoComponent theme="dark" />
       <PowerButton theme="dark" />
       <Socials theme="dark" />
-
-      <Ground>
-        <Line width={500} height={25} />
-        <Line width={500} height={25} />
-        <Line width={500} height={25} />
-      </Ground>
-
-      <User>
-        <img src={Girl} alt='girl' />
-      </User>
+      <div className="carouselContainer">
+        <Carousel>
+          <div className="carouselImg">
+            <img src={Work1} alt="work"/>
+            <p className="legend">Legend 1</p>
+          </div>
+          <div className="carouselImg">
+            <img src={Work2} alt="work" />
+            <p className="legend">Legend 2</p>
+          </div>
+          <div className="carouselImg">
+            <img src={Work3} alt="work" />
+            <p className="legend">Legend 3</p>
+          </div>
+          <div className="carouselImg"> 
+            <img src={Work4} alt="work" />
+            <p className="legend">Legend 3</p>
+          </div>
+          <div className="carouselImg">
+            <img src={Work5} alt="work" />
+            <p className="legend">Legend 3</p>
+          </div>
+        </Carousel>
+      </div>
     </MainContainer>
   );
 }
